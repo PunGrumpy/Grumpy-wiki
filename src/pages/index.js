dc5b31f0f5637ddf2ea9ac5cea0d90febd55f56f
PunGrumpy <noppakorn.pun@gmail.com>
2022-08-14 20:09:21 +0700

    modified:   src/pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ function MyHero() {
                     Always <br /> For Freedom.
                 </h1>
                 <p className={styles.leftContainer_p}>
-                    A guy who tried of coding 💨
+                    A guy who frustrated with coding 💨
                     <br />
                     Records the knowledge here, I hope it will help you.
                 </p>
