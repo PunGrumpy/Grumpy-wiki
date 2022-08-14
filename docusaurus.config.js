4a90c6dcab7e54cfc07a70f9aa122a6adb160eab
PunGrumpy <noppakorn.pun@gmail.com>
2022-08-14 19:58:23 +0700

    modified:   docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,17 +56,17 @@ module.exports = {
                 },
             ],
             navbar: {
-                title: "Grumpy's Wiki",
+                title: "👻 Grumpy's Wiki",
                 hideOnScroll: true,
-                logo: {
-                    alt: 'Site Logo',
-                    src: 'img/ghost.png',
-                    srcDark: 'img/ghost-dark.png',
-                    href: 'https://grumpy-wiki.netlify.app/',
-                    target: '_self',
-                    width: 32,
-                    height: 32,
-                },
+                // logo: {
+                //     alt: 'Site Logo',
+                //     src: 'img/ghost.png',
+                //     srcDark: 'img/ghost-dark.png',
+                //     href: 'https://grumpy-wiki.netlify.app/',
+                //     target: '_self',
+                //     width: 32,
+                //     height: 32,
+                // },
                 items: [
                     {
                         position: 'right',
